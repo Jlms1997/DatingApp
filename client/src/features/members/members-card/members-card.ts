581ae96dab9175d8c55db987f291d3d0fbbcb71c
Jlms1997 <jlm26520447@gmail.com>
2025-08-31 22:59:40 -0300

    User Interface

---
+++ client/src/features/members/members-card/members-card.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { Member } from '../../../types/member';
+import { RouterLink } from '@angular/router';
+import { AgePipe } from '../../../core/pipes/age-pipe';
+
+@Component({
+  selector: 'app-members-card',
+  imports: [RouterLink, AgePipe],
+  templateUrl: './members-card.html',
+  styleUrl: './members-card.css'
+})
+export class MembersCard {
+ member = input.required<Member>();
+
+}
